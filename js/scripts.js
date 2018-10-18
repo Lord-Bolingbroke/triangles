761c4337714fa03541ea42a6827fecc882694eb7
Jared Reando and Noah Schooler <jared@>
2018-10-18 16:32:07 -0700

    tweak JS formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#lengths").submit(function(event) {
+    event.preventDefault();
     // $("p").hide();
 
     var side1 = parseInt($("#side1").val());
@@ -11,33 +12,34 @@ $(document).ready(function() {
     $(".side3").text(side3);
 
 
-  if ((side1 + side2) <= side3)  {
-    $("p#answer").text("NOT A TRIANGLE");
-  }
-  else if ((side2 + side3) <= side1)  {
-    $("p#answer").text("NOT A TRIANGLE");
-  }
-  else if ((side1 + side3) <= side2)  {
-    $("p#answer").text("NOT A TRIANGLE");
-  }
-  // equilateral
-  else if ((side1 === side2) && (side2 === side3)) {
-    // $("#equilateral").show();
-    $("p#answer").html("Equilateral");
-  }
-  // scalene
-  else if ((side1 != side2 && side1 != side3) && (side3 != side2)) {
-    $("p#answer").text("Scalene");
-  }
-// isocelese
-  else if (side1 != side3) {
-    $("p#answer").text("Isosceles");
-  }
-  else if (side3 != side2) {
-    $("p#answer").text("Isosceles");
-  }
+    if ((side1 + side2) <= side3)  {
+      $("p#answer").text("NOT A TRIANGLE");
+    }
+    else if ((side2 + side3) <= side1)  {
+      $("p#answer").text("NOT A TRIANGLE");
+    }
+    else if ((side1 + side3) <= side2)  {
+      $("p#answer").text("NOT A TRIANGLE");
+    }
+    // equilateral
+    else if ((side1 === side2) && (side2 === side3)) {
+      // $("#equilateral").show();
+      $("p#answer").text("Equilateral");
+    }
+    // scalene
+    else if ((side1 != side2 && side1 != side3) && (side3 != side2)) {
+      $("p#answer").text("Scalene");
+    }
+  // isocelese
+    else if (side1 != side3) {
+      $("p#answer").text("Isosceles");
+    }
+    else if (side3 != side2) {
+      $("p#answer").text("Isosceles");
+    } else {
+      $("p#answer").text("Whelp. You stumped us.")
+    }
 
-    event.preventDefault();
 
   });
 });
